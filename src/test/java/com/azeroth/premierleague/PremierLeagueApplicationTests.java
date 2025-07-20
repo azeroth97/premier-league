d83ac03fe9859fecd5049dea8474aa79f3bfe2ef
azeroth97 <aazeroth97@gmail.com>
2025-07-20 11:05:44 +0700

    initial commit

---
+++ src/test/java/com/azeroth/premierleague/PremierLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azeroth.premierleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
